47efe5ce69e6c7a98c805208fc3848a4ebe78bf6
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2023-09-21 18:15:03 +0300

    refactor

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -7,18 +7,18 @@ import bary.BarySystem;
 //
 public class Data {
     public static final BaryObject BARY_SYSTEM;
-    private static final BaryObject BARY_OBJECT_1, BARY_OBJECT_2, BARY_OBJECT_3;
+    private static final BaryObject BARY_BODY_1, BARY_BODY_2, BARY_BODY_3;
 
     static {
         BARY_SYSTEM = new BarySystem(null);
 
-        BARY_OBJECT_1 = new BaryBody((BarySystem) BARY_SYSTEM, 0, 0);
-        BARY_OBJECT_2 = new BaryBody((BarySystem) BARY_SYSTEM, 0, 0);
-        BARY_OBJECT_3 = new BaryBody((BarySystem) BARY_SYSTEM, 0, 0);
+        BARY_BODY_1 = new BaryBody((BarySystem) BARY_SYSTEM, 0, 0);
+        BARY_BODY_2 = new BaryBody((BarySystem) BARY_SYSTEM, 0, 0);
+        BARY_BODY_3 = new BaryBody((BarySystem) BARY_SYSTEM, 0, 0);
 
-        ((BarySystem) BARY_SYSTEM).addObject(BARY_OBJECT_1);
-        ((BarySystem) BARY_SYSTEM).addObject(BARY_OBJECT_2);
-        ((BarySystem) BARY_SYSTEM).addObject(BARY_OBJECT_3);
+        ((BarySystem) BARY_SYSTEM).addObject(BARY_BODY_1);
+        ((BarySystem) BARY_SYSTEM).addObject(BARY_BODY_2);
+        ((BarySystem) BARY_SYSTEM).addObject(BARY_BODY_3);
         System.out.println("New BarySystem created.");
     }
 }
